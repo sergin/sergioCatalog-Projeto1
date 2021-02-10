5e0b293ca19e901974955c53aa4e5065ec81375e
Sérgio Alves <sergioalves.ti@gmail.com>
2021-02-10 14:17:40 -0300

    Project Created

---
+++ backend/src/main/java/catalog/com/sergiocatalog/SergiocatalogApplication.java
@@ -0,0 +1,13 @@
+package catalog.com.sergiocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergiocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergiocatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/catalog/com/sergiocatalog/SergiocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package catalog.com.sergiocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergiocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

913da12f3293dcb358eccb57af91c3efd62aaf97
Sérgio Alves <sergioalves.ti@gmail.com>
2021-03-09 21:03:14 -0300

    Database seeding

--- backend/src/main/java/catalog/com/sergiocatalog/services/CategoryService.java
+++ backend/src/main/java/catalog/com/sergiocatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import catalog.com.sergiocatalog.entities.Category;
 //import catalog.com.sergiocatalog.resources.CategoryResource;
@@ -15,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional
 	public List<Category> findAll(){
 		return repository.findAll();
 	

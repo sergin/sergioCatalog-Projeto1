6b8cf7d56cd4eebad688fa803bb2c7e9ca1531c1
Sérgio Alves <sergioalves.ti@gmail.com>
2021-03-16 21:14:40 -0300

    Auditing data

--- backend/src/main/java/catalog/com/sergiocatalog/entities/Category.java
+++ backend/src/main/java/catalog/com/sergiocatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package catalog.com.sergiocatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +21,12 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant UPDATEdAt;
 	
 	public Category() {
 		
@@ -45,6 +55,26 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public Instant getUPDATEdAt() {
+		return UPDATEdAt;
+	}
+
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		UPDATEdAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (name) VALUES ('Books');
-INSERT INTO tb_category (name) VALUES ('Electronicks');
-INSERT INTO tb_category (name) VALUES ('Computes');
+INSERT INTO tb_category (name, created_At) VALUES ('Books', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Electronicks', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Computes', NOW());
